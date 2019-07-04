df3a7d484d476f8d39504da8cf36adace9a38e6e
Chad Smith <chad.e.smith@outlook.com>
2019-07-04 00:22:53 -0500

    tweak monster attribute calculation

--- MonsterEngine/Services/MonsterService.cs
+++ MonsterEngine/Services/MonsterService.cs
@@ -18,13 +18,13 @@ namespace MonsterEngine.Services
             int baseValue = PlayerLevel * 10;
             int maxLife = 90 + baseValue;
             int life = 50 + baseValue - addPoints;
-            int hitChance = (50 + baseValue) - addPoints;
-            int block = (50 + baseValue) + addPoints;
+            int hitChance = (50 + (baseValue / 200)) - addPoints;
+            int block = (50 + (baseValue / 200)) + addPoints;
             int minDamage = 20 + PlayerLevel + addPoints;
             int maxDamage = (minDamage * 2) + addPoints;
             string description = DescriptionHelper.GenerateDescription();
             string name = Name.GetName();
-            bool hasPowerfulNose = hitChance % 2 > 0 ? true : false;
+            bool hasPowerfulNose = hitChance % 3 == 0 ? true : false;
 
             Monster getMonster = new Monster(name, life, maxLife, hitChance, block, minDamage, maxDamage, description, hasPowerfulNose);
 
